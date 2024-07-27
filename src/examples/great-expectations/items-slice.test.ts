03af152c779c8edc0b52ce2221102b93e029dd4f
xvin <xvin@bupt.edu.cn>
2024-07-27 15:16:48 +0800

    Add some unit test cases

---
+++ src/examples/great-expectations/items-slice.test.ts
@@ -0,0 +1,115 @@
+import reducer, {
+  add,
+  remove,
+  toggle,
+  markAllAsUnpacked,
+  update,
+} from './items-slice';
+
+it('returns an empty array as the initial state', () => {
+  expect(reducer(undefined, { type: 'noop' })).toEqual([]);
+});
+
+it('supports adding an item with the correct name', () => {
+  expect.hasAssertions();
+  const result = reducer([], add({ name: 'iPhone' }));
+
+  expect(result).toEqual([expect.objectContaining({ name: 'iPhone' })]);
+});
+
+it('prefixes ids with "item-"', () => {
+  expect.hasAssertions();
+  const result = reducer([], add({ name: 'iPhone' }));
+
+  expect(result).toEqual([
+    expect.objectContaining({
+      name: 'iPhone',
+      id: expect.stringMatching(/^item-/),
+    }),
+  ]);
+});
+
+it('defaults new items to a packed status of false', () => {
+  expect.hasAssertions();
+  const result = reducer([], add({ name: 'iPhone' }));
+
+  expect(result.length).toBe(1);
+  expect(result).toEqual([
+    expect.objectContaining({
+      packed: false,
+    }),
+  ]);
+});
+
+it('supports removing an item', () => {
+  expect.hasAssertions();
+  const state = [
+    {
+      id: '1',
+      name: 'iPhone',
+      packed: false,
+    },
+  ];
+
+  const result = reducer(state, remove({ id: '1' }));
+
+  expect(result.length).toBe(0);
+  expect(result).not.toContainEqual(expect.objectContaining({ id: '1' }));
+});
+
+it('supports toggling an item', () => {
+  expect.hasAssertions();
+  const state = [
+    {
+      id: '1',
+      name: 'iPhone',
+      packed: false,
+    },
+  ];
+
+  const result = reducer(state, toggle({ id: '1' }));
+
+  expect(result.length).toBe(1);
+  expect(result).toContainEqual(
+    expect.objectContaining({ id: '1', packed: true }),
+  );
+});
+
+it('supports updating an item', () => {
+  expect.hasAssertions();
+  const state = [
+    {
+      id: '1',
+      name: 'iPhone',
+      packed: false,
+    },
+  ];
+
+  const result = reducer(
+    state,
+    update({ id: '1', name: 'Samsung Galaxy S23' }),
+  );
+
+  expect(result).toContainEqual(
+    expect.objectContaining({ id: '1', name: 'Samsung Galaxy S23' }),
+  );
+});
+
+it.todo('supports marking all items as unpacked', () => {
+  expect.hasAssertions();
+  const state = [
+    {
+      id: '1',
+      name: 'iPhone',
+      packed: true,
+    },
+    {
+      id: '2',
+      name: 'iPhone Charger',
+      packed: true,
+    },
+  ];
+
+  const result = reducer(state, markAllAsUnpacked());
+  expect(result).not.toContainEqual(expect.objectContaining({ packed: true }));
+});
